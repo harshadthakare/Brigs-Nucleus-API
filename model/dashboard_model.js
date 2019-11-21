454e2071c8abddce4ba2e71d096eee06c0cc6bae
harshadthakare <harshad@stackmint.com>
2019-11-21 15:37:07 +0530

    commit on 22/11

--- model/dashboard_model.js
+++ model/dashboard_model.js
@@ -20,7 +20,7 @@ class Dashboard {
         let sql = `SELECT a.assetId,a.assetCode,assetTitle,a.modelNumber,a.description,CONCAT('${BASE_URL}','',a.image) as assetImage,c.title 
                     FROM asset a 
                     LEFT JOIN donechecklist d ON d.assetIdFK = a.assetId 
-                    LEFT JOIN assetcatrelation a1 ON a1.assetIdFK = a.as    setId 
+                    LEFT JOIN assetcatrelation a1 ON a1.assetIdFK = a.assetId 
                     LEFT JOIN category c ON a1.categoryIdFK = c.categoryId 
                     WHERE d.assetIdFK IS NULL AND a.organizationIdFK = 1 AND a.isDeleted = 0 `
         return sql;
